71b07edd9e7f8683af20c7d97b83ccc39ed70fc0
chinwuba Israel <chiizzy5@gmail.com>
2022-12-02 10:45:08 +0100

    h

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Navbar, Footer } from "./components";
+import { Navbar, Footer, RightSideBar } from "./components";
 import { Home, Feed, Login, SignUp, Root, EditProfile, Post, Profile, } from './routes';
 
 
@@ -11,7 +11,7 @@ const App = () => (
   <div className="bg-primary w-full overflow-hidden">
       <QueryClientProvider client={client}>
         <Router>
-          <Navbar />
+          <RightSideBar />
           <Routes>
             <Route path="/" element={<Root />} />
             <Route path="/profile" element={<Profile />} />
--- client/vite-project/src/components/RightSideBar.jsx
+++ client/vite-project/src/components/RightSideBar.jsx
@@ -1,8 +1,30 @@
+import RightSideBarData from "./RightSideBarData";
+import { Link } from "react-router-dom";
 
 
+  
 const RightSideBar = () => {
+
+
   return (
-    <div>RightSideBar</div>
+    <div className={`w-72 h-screen fixed bg-darkPurple `}>
+        <div className={`flex gap-x-4 items-center`}>
+            <Link className={`font-poppins text-gradient font-bold cursor-pointer text-[30px]`} to="/">9jaConnect</Link>
+        </div>
+
+        <ul className="pt-6">
+            {
+                RightSideBarData.map((item, index) => (
+                    <li key={index} className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 mt-2 `}>
+                        <Link className="flex" to={item.path}>
+                            {item.icon}
+                            <span className="ml-4">{item.title}</span>
+                        </Link>
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
   )
 }
 
